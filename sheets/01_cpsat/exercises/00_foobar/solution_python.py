6eb8261e478a1b51e28353ce4d7f74345dd03a87
Christian <c.bognitz@tu-braunschweig.de>
2024-04-10 20:45:49 +0200

    Implement 00_foobar in pure python

--- sheets/01_cpsat/exercises/00_foobar/solution_python.py
+++ sheets/01_cpsat/exercises/00_foobar/solution_python.py
@@ -6,8 +6,10 @@ def solve(instance: Instance) -> Solution:
     Implement your solver for the problem here!
     """
     numbers = instance.numbers
+    maximum = max(numbers)
+    minimum = min(numbers)
     return Solution(
-        number_a=numbers[0],
-        number_b=numbers[-1],
-        distance=abs(numbers[0] - numbers[-1]),
+        number_a=maximum,
+        number_b=minimum,
+        distance=abs(maximum - minimum),
     )

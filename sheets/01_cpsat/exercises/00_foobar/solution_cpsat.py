c78d6181a0307ecf5b9a800ff53d20f8bb0fa968
Christian <c.bognitz@tu-braunschweig.de>
2024-04-10 21:51:23 +0200

    00_foobar initial cp_sat solution

--- sheets/01_cpsat/exercises/00_foobar/solution_cpsat.py
+++ sheets/01_cpsat/exercises/00_foobar/solution_cpsat.py
@@ -8,8 +8,45 @@ def solve(instance: Instance) -> Solution:
     """
     numbers = instance.numbers
     model = cp_model.CpModel()
+
+    # IntVars for the minimum and maximum
+    minimum = model.NewIntVar(-100, 100, "minimum")
+    maximum = model.NewIntVar(-100, 100, "maximum")
+
+    # minmum should be smaller than maximum
+    model.Add(minimum <= maximum)
+
+    for num in numbers:
+        model.Add(minimum <= num) # the minimum is smaller (or equal) to all the other numbers
+        model.Add(maximum >= num) # the maximum is larger (or equal) to all the other numbers
+
+    # A boolean variable for each Value. To select if this is the minimum value.
+    mins = [model.NewBoolVar(f"min_{i}") for i in range(len(numbers))]
+
+    # A boolean variable for each Value. To select if this is the maximum value.
+    maxs = [model.NewBoolVar(f"max_{i}") for i in range(len(numbers))]
+
+    # Only 1 minimum can exist
+    model.Add(sum(mins) == 1)
+    # Only 1 maximum can exist
+    model.Add(sum(maxs) == 1) 
+
+    # maximize the distance between any 2 numbers.
+    model.add(minimum == sum(x * i for x, i in zip(mins, numbers)))
+
+    model.Add(maximum ==  sum(x * i for x, i in zip(maxs, numbers)))
+
+    # find the actual largest difference.
+    model.Maximize(maximum - minimum)
+
+    solver = cp_model.CpSolver()
+    solver.parameters.log_search_progress = True
+    status = solver.Solve(model)
+
+    assert status == cp_model.OPTIMAL
+
     return Solution(
-        number_a=numbers[0],
-        number_b=numbers[-1],
-        distance=abs(numbers[0] - numbers[-1]),
+        number_a=solver.Value(minimum),
+        number_b=solver.Value(maximum),
+        distance=abs(solver.Value(minimum) - solver.Value(maximum)),
     )

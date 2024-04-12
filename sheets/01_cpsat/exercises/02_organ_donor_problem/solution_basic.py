72a29d97652479e78ba9903be0cb909a3be14409
Christian <c.bognitz@tu-braunschweig.de>
2024-04-13 00:05:50 +0200

    02_organ_donor_problem: basic solution implemented

--- sheets/01_cpsat/exercises/02_organ_donor_problem/solution_basic.py
+++ sheets/01_cpsat/exercises/02_organ_donor_problem/solution_basic.py
@@ -13,10 +13,34 @@ class CrossoverTransplantSolver:
         :param Database database: The organ donor/recipients database.
         """
         self.database = database
-        # TODO: Implement me!
+        recipients = self.database.get_all_recipients()
 
+        self.model = CpModel()
         self.solver = CpSolver()
         self.solver.parameters.log_search_progress = True
+
+        # donors - e_{i, j} if the donor is making a donation for recipiant i to recipiant j
+        #          aka if the edge (i, j) is on the cycle
+        self.donors = {(src.id, dst.id): (donor, dst, self.model.NewBoolVar(f"e_{src.id}_{dst.id}")) for src in recipients for donor in self.database.get_partner_donors(src) for dst in self.database.get_compatible_recipients(donor) }
+
+        
+        # No self loops allowed (optional: the problem alredy states, that a recipiants donor are all incompatible)
+        for (i, j), (_, _, donoation) in self.donors.items():
+            if i == j:
+                self.model.add(donoation == 0)
+        
+        # only at most 1 donation for each recpiants partner donors
+        for src in recipients:
+            self.model.add(sum([donoation for (i, j), (_, _, donoation) in self.donors.items() if i == src.id]) <= 1)
+
+        # if a recpiant recives a donation the partner donors have to donate.
+        # the recieved amount of donations must equal the outgoing ammount of donations.
+        for recipient in recipients:
+            self.model.add(sum([donoation for (i, j), (_, _, donoation) in self.donors.items() if j == recipient.id]) == sum([donoation for (i, j), (_, _, donoation) in self.donors.items() if i == recipient.id]))
+
+
+        # Maximize the length of the cycle aka the ammount of donations
+        self.model.maximize(sum(donoation for (_, _, donoation) in self.donors.values()))
 
 
     def optimize(self, timelimit: float = math.inf) -> Solution:
@@ -29,4 +53,9 @@ class CrossoverTransplantSolver:
             return Solution(donations=[])
         if timelimit < math.inf:
             self.solver.parameters.max_time_in_seconds = timelimit
-        # TODO: Implement me!
+        
+        status = self.solver.Solve(self.model)
+        assert status == OPTIMAL
+
+        return Solution(donations=[Donation(donor=donor, recipient=recipient) for (i, j), (donor, recipient, donation) in self.donors.items() if self.solver.Value(donation) ])
+

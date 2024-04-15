482dc43a7c791394aaaa30258cad15bf3382197d
Christian <c.bognitz@tu-braunschweig.de>
2024-04-15 20:28:54 +0200

    02_organ_donor_problem: small_cycles: rewrite to use recipiants as decision variables for each cycle

--- sheets/01_cpsat/exercises/02_organ_donor_problem/solution_small_cycles.py
+++ sheets/01_cpsat/exercises/02_organ_donor_problem/solution_small_cycles.py
@@ -16,55 +16,62 @@ class CycleLimitingCrossoverTransplantSolver:
 
         self.database = database
         recipients = self.database.get_all_recipients()
-        max_cycles = (len(recipients) + 1) // 2 # each cycles needs at least 2 nodes
+        max_cycles = len(recipients) // 2 # each cycles needs at least 2 nodes
 
         self.model = CpModel()
         self.solver = CpSolver()
         self.solver.parameters.log_search_progress = True
 
-        # donations - (Donor, Recipient, e_{i, j}, [c_{c}_{i, j}])
+        # donations - (Donor, Recipient, e_{i, j})
         #   e_{i, j}     - if the donor is making a donation for recipiant i to recipiant j
         #                  aka if the edge (i, j) is on the cycle
-        #   c_{c}_{i, j} - describes, if the donation i->j is on cycles c.
-        self.donations = {(src.id, dst.id): (donor, dst, self.model.NewBoolVar(f"e_{src.id}_{dst.id}"), [self.model.NewBoolVar(f"c_{c}_{src.id}-{dst.id}") for c in range(max_cycles)]) for src in recipients for donor in self.database.get_partner_donors(src) for dst in self.database.get_compatible_recipients(donor) }
+        self.donations = {(src.id, dst.id): (donor, dst, self.model.NewBoolVar(f"e_{src.id}_{dst.id}")) for src in recipients for donor in self.database.get_partner_donors(src) for dst in self.database.get_compatible_recipients(donor) }
 
         # No self loops allowed (optional: the problem alredy states, that a recipiants donor are all incompatible)
-        for (i, j), (_, _, donoation, _) in self.donations.items():
+        for (i, j), (_, _, donoation) in self.donations.items():
             if i == j:
                 self.model.Add(donoation == 0)
         
-        # only at most 1 donation for each recpiants partner donors
+        # at most 1 donation for each recpiants partner donors
         for src in recipients:
-            self.model.Add(sum([donoation for (i, j), (_, _, donoation, _) in self.donations.items() if i == src.id]) <= 1)
+            self.model.Add(sum([donoation for (i, j), (_, _, donoation) in self.donations.items() if i == src.id]) <= 1)
+
+        # at most 1 donation for each recpiant (optional: covered by the constraint below)
+        for src in recipients:
+            self.model.Add(sum([donoation for (i, j), (_, _, donoation) in self.donations.items() if j == src.id]) <= 1)
 
         # if a recpiant recives a donation the partner donors have to donate.
         # the recieved amount of donations must equal the outgoing ammount of donations.
         for recipient in recipients:
-            self.model.Add(sum([donoation for (i, j), (_, _, donoation, _) in self.donations.items() if j == recipient.id]) == sum([donoation for (i, j), (_, _, donoation, _) in self.donations.items() if i == recipient.id]))
+            self.model.Add(sum([donoation for (i, j), (_, _, donoation) in self.donations.items() if j == recipient.id]) == sum([donoation for (i, j), (_, _, donoation) in self.donations.items() if i == recipient.id]))
 
 
-        # each edge corresponds to a donation
-        for (donor, dst, donation, cycles) in self.donations.values():
-            self.model.Add(sum(cycles) == donation)
-            # a donation can lie on at most one cycle (optional: included in the above constraint)
-            self.model.Add(sum(cycles) <= 1)
+        # cycles - c_{c}_{r} describes, if the recipient r is on cycles c.
+        self.cycles = {r.id: [self.model.NewBoolVar(f"c_{r}_{c}") for c in range(max_cycles)] for r in recipients}
 
-        # a donation is on the same cycle if it is on the same path
+        # a recipient can lie on at most one cycle (optional: covered by the condition below)
         for recipient in recipients:
-            donations_in  = [cycles for (i, j), (_, _, donoation, cycles) in self.donations.items() if j == recipient.id]
-            donations_out = [cycles for (i, j), (_, _, donoation, cycles) in self.donations.items() if i == recipient.id]
+            self.model.Add(sum(self.cycles[recipient.id]) <= 1)
 
-            for cycle in range(max_cycles):
-                self.model.Add(sum([donations_in_cycles[cycle] for donations_in_cycles in donations_in]) == sum([donations_out_cyces[cycle] for donations_out_cyces in donations_out]))
+        # A recipient must lie on a cycle, if someone donates to him
+        for recipient in recipients:
+            self.model.Add(sum([donoation for (i, j), (_, _, donoation) in self.donations.items() if j == recipient.id]) == sum(self.cycles[recipient.id]))
 
+        # recipients are on the same cycle, if there is a donation between those 2
+        for recipient1 in recipients:
+            for recipient2 in recipients:
+                donation = self.donations.get((recipient1.id, recipient2.id))
+                if donation is not None:
+                    for c1, c2 in zip(self.cycles[recipient1.id], self.cycles[recipient2.id]):
+                        self.model.Add(c1 == c2).OnlyEnforceIf(donation[2])
 
         # each cycle can contain at most 3 donations
         for cycle in range(max_cycles):
-            self.model.Add(sum(cycles[cycle] for (_, _, _, cycles) in self.donations.values()) <= 3)
+            self.model.Add(sum([self.cycles[recipient.id][cycle] for recipient in recipients]) <= 3)
 
 
         # Maximize the length of the cycle aka the ammount of donations
-        self.model.Maximize(sum(donoation for (_, _, donoation, _) in self.donations.values()))
+        self.model.Maximize(sum(donoation for (_, _, donoation) in self.donations.values()))
 
 
     def optimize(self, timelimit: float = math.inf) -> Solution:
@@ -76,5 +83,5 @@ class CycleLimitingCrossoverTransplantSolver:
         status = self.solver.Solve(self.model)
         assert status == OPTIMAL
 
-        return Solution(donations=[Donation(donor=donor, recipient=recipient) for (donor, recipient, donation, _) in self.donations.values() if self.solver.Value(donation) ])
+        return Solution(donations=[Donation(donor=donor, recipient=recipient) for (donor, recipient, donation) in self.donations.values() if self.solver.Value(donation) ])
 

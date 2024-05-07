85b25a957723a7b4146007f3fc9f51d807291e73
Christian <c.bognitz@tu-braunschweig.de>
2024-05-07 14:54:44 +0200

    wip: 04: 01: TSP Relaxation

--- sheets/04_mip/exercises/01_tsp/solution_relaxation.py
+++ sheets/04_mip/exercises/01_tsp/solution_relaxation.py
@@ -8,11 +8,70 @@ import gurobipy as gp
 import networkx as nx
 
 
+class _EdgeVariables:
+    """
+    A helper class that manages the variables for the edges.
+    Such a helper class turns out to be useful in many cases.
+    """
+    EPSILON = 0.01 # if the weight of an edge exceeds this value it is considered selected.
+
+    def __init__(self, G: nx.Graph, model: gp.Model):
+        self._graph = G
+        self._model = model
+        self._vars = {
+            (u, v): model.addVar(vtype=gp.GRB.CONTINUOUS, name=f"edge_{u}_{v}")
+            for u, v in G.edges
+        }
+
+    def x(self, v, w) -> gp.Var:
+        """
+        Return variable for edge (v, w).
+        """
+        if (v, w) in self._vars:
+            return self._vars[v, w]
+        # If (v,w) was not found, try (w,v)
+        return self._vars[w, v]
+
+    def outgoing_edges(self, vertices):
+        """
+        Return all edges&variables that are outgoing from the given vertices.
+        """
+        # Not super efficient, but efficient enough for our purposes.
+        for (v, w), x in self._vars.items():
+            if v in vertices and w not in vertices:
+                yield (v, w), x
+            elif w in vertices and v not in vertices:
+                yield (w, v), x
+
+    def incident_edges(self, v):
+        """
+        Return all edges&variables that are incident to the given vertex.
+        """
+        for n in self._graph.neighbors(v):
+            yield (v, n), self.x(v, n)
+
+    def __iter__(self):
+        """
+        Iterate over all edges&variables.
+        """
+        return iter(self._vars.items())
+
+    def as_graph(self, in_callback: bool = False):
+        """
+        Return the current solution as a graph.
+        """
+        if in_callback:
+            # If we are in a callback, we need to use the solution from the callback.
+            used_edges = [(*vw, {"x": x.X}) for vw, x in self if self._model.cbGetSolution(x) >= self.EPSILON]
+        else:
+            # Otherwise, we can use the solution from the model.
+            used_edges = [(*vw, {"x": x.X}) for vw, x in self if x.X >= self.EPSILON]
+        return nx.Graph(used_edges)
 
 
 class GurobiTspRelaxationSolver:
     """
-    IMPLEMENT ME!
+    A TSP Relaxation Solver in Gurobi
     """
 
     def __init__(self, G: nx.Graph):
@@ -28,14 +87,23 @@ class GurobiTspRelaxationSolver:
             weight > 0 for _, _, weight in G.edges.data("weight", default=None)
         ), "Invalid graph"
         self._model = gp.Model()
-        # TODO: Implement me!
+        self._edge_vars = _EdgeVariables(self.graph, self._model)
+        self._constarint_two_incident_edges_per_node()
+        self._minimize_cycle_length()
 
+    def _constarint_two_incident_edges_per_node(self):
+        for u in self.graph.nodes:
+            self._model.addConstr(sum(x for _, x in self._edge_vars.incident_edges(u)) == 2)
+
+    def _minimize_cycle_length(self):
+        self._model.setObjective(sum(self.graph.edges[edge]["weight"] * x for edge, x in self._edge_vars), gp.GRB.MINIMIZE)
 
     def get_lower_bound(self) -> float:
         """
         Return the current lower bound.
         """
-        # TODO: Implement me!
+        # Only works if the model has been optimized once.
+        return self.model.ObjBound
 
     def get_solution(self) -> typing.Optional[nx.Graph]:
         """
@@ -51,13 +119,17 @@ class GurobiTspRelaxationSolver:
         graph.add_edge(1, 2, x=1.0)
         ```
         """
-        # TODO: Implement me!
+        # Only try to get a solution, if the model has been solved optimally
+        assert self._model.status == gp.GRB.OPTIMAL
+        solution_graph = self._edge_vars.as_graph()
+        return solution_graph
 
     def get_objective(self) -> typing.Optional[float]:
         """
         Return the objective value of the last solution.
         """
-        # TODO: Implement me!
+        # Only works if the model has been optimized once.
+        return self._model.ObjVal
 
     def solve(self) -> None:
         """
@@ -66,4 +138,19 @@ class GurobiTspRelaxationSolver:
         # Set parameters for the solver.
         self._model.Params.LogToConsole = 1
 
-        # TODO: Implement me!
+        # Solve the model the first time
+        self._model.optimize()
+
+        # Perform multiple iterations.  In each iteration, check if
+        # the graph is disconnected, add constraints and omtimize again.
+        while not nx.is_connected(self._edge_vars.as_graph()):
+            solution = self._edge_vars.as_graph()
+            for component in nx.connected_components(solution):
+                # we have a disconnected component, add a constraint to connect it
+                self._model.addConstr(
+                    sum(x for _, x in self._edge_vars.outgoing_edges(component)) >= 2*self._edge_vars.EPSILON
+                )
+            # reoptimize, with the added Constraints.
+            self._model.optimize()
+
+        assert self._model.Status == gp.GRB.OPTIMAL

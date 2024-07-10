77f54831138f7fa9f9b0e74f24f1bd47a9488fb7
Julian Paulat <jupaulat@gmail.com>
2024-07-10 13:12:27 +0200

    Added StreamlitLogger to print stats to GUI

--- project/pages/Solver.py
+++ project/pages/Solver.py
@@ -6,7 +6,10 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import time
+import sys
+import io
 from solver_process import SEPSolverProcess
+from solver import SolutionStatCalculator
 from data_schema import Instance
 
 # Custom CSS for Text components
@@ -15,22 +18,20 @@ st.write("""
     [data-testid=stText] {
         border-radius: 0.5em;
         padding: 1rem;
-	    background: rgb(26, 28, 36);
+        background: rgb(26, 28, 36);
     }
     </style>
-    """,unsafe_allow_html=True)
+    """, unsafe_allow_html=True)
 
 # Load data if not available
-
 if any(var not in st.session_state for var in ['projects', 'students', 'languages']):
     st.switch_page("Configurator.py")
 
 st.title("SEP Solver")
 
-
 st.subheader("Configuration")
 with st.form("Config", border=False):
-    test_config_option = st.number_input("Test Congfig option", min_value=0, value=100)
+    test_config_option = st.number_input("Test Config option", min_value=0, value=100)
 
     c1, c2 = st.columns([0.75, 0.25])
     solve_button = c1.form_submit_button("Solve", type="primary", use_container_width=True)
@@ -53,6 +54,18 @@ if "log_text" not in st.session_state:
     st.session_state.log_text = ""
 log_placeholder.text(st.session_state.log_text)
 
+class StreamlitLogger(io.StringIO):
+    def __init__(self):
+        super().__init__()
+        self.stdout = sys.stdout
+        sys.stdout = self
+
+    def close(self):
+        sys.stdout = self.stdout
+        super().close()
+
+    def get_value(self):
+        return self.getvalue()
 
 if solve_button:
     with progress_spinner, st.spinner("Calculating Solution..."):
@@ -60,30 +73,40 @@ if solve_button:
         projects = st.session_state.projects
         students = st.session_state.students
         languages = st.session_state.languages
-        # instance = Generator().parse_gui_data(projects, students, languages)
         with open("./instances/anonymized_data_1.json") as f:
             instance = Instance.model_validate_json(f.read())
-
 
         solver_process = SEPSolverProcess(instance)
         solver_process.start()
 
         st.session_state.log_text = ""
         counter = 0
-        while True:
-            logs = solver_process.get_log()            
-            if logs:
-                st.session_state.log_text += "".join(logs) + "\n"
-            log_placeholder.text(st.session_state.log_text)
 
-            if not solver_process.is_running() or abort_button:
-                if abort_button:
-                    solver_process.interrupt()
-                    status_placeholder.warning("Solution process interrupted.")
-                else:
-                    solution = solver_process.get_solution()
-                    st.session_state.solution = pd.DataFrame(solution)
-                    solution_placeholder.dataframe(st.session_state.solution, use_container_width=True)
-                    status_placeholder.success("Solver finished.")
-                break
-            time.sleep(0.1)
+        logger = StreamlitLogger()
+
+        try:
+            while True:
+                logs = solver_process.get_log()
+                if logs:
+                    st.session_state.log_text += "".join(logs) + "\n"
+                st.session_state.log_text += logger.get_value()
+                log_placeholder.text(st.session_state.log_text)
+
+                if not solver_process.is_running() or abort_button:
+                    if abort_button:
+                        solver_process.interrupt()
+                        status_placeholder.warning("Solution process interrupted.")
+                    else:
+                        solution = solver_process.get_solution()
+                        sol_stat_calculator = SolutionStatCalculator(instance, solution)
+                        sol_stat_calculator.printStats()
+                        st.session_state.solution = pd.DataFrame(solution)
+                        solution_placeholder.dataframe(st.session_state.solution, use_container_width=True)
+                        status_placeholder.success("Solver finished.")
+                    break
+                time.sleep(0.1)
+        finally:
+            remaining_logs = logger.get_value()
+            logger.close()
+            st.session_state.log_text += remaining_logs
+            log_placeholder.text(st.session_state.log_text)
